069e1bd549b204ef1253399eadc4c2d86b6d7892
Csorbert <csorbert404@gmail.com>
2023-09-25 17:24:24 +0200

    Update Szimulacio.cs

--- GameOfLife/GameOfLife/Szimulacio.cs
+++ GameOfLife/GameOfLife/Szimulacio.cs
@@ -8,5 +8,36 @@ namespace GameOfLife
 {
     internal class Szimulacio
     {
+<<<<<<< Updated upstream
+=======
+
+        private readonly Palya palya;
+
+        private int KorokSzama { get; set; }
+
+        private int JelenlegiKorSzama { get; set; }
+
+        public Szimulacio(Palya palya, int korokSzama)
+        {
+
+            this.palya = palya;
+            KorokSzama = korokSzama;
+
+        }
+
+        public void SzimulacioInditas()
+        {
+            for (JelenlegiKorSzama = 1; JelenlegiKorSzama <= KorokSzama; JelenlegiKorSzama++)
+            {
+                Console.WriteLine($"{JelenlegiKorSzama}. kör lefutott");
+            }
+        }
+
+        public int LekerJelenlegiKorSzama()
+        {
+            return JelenlegiKorSzama;
+        }
+
+>>>>>>> Stashed changes
     }
 }
