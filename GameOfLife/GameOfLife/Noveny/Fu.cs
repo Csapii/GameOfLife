f26038fa09a2e27f1adb4a885681579a786894c5
Csapii <mail.szasz.csaba@gmail.com>
2023-09-30 12:28:19 +0200

    tápérték növelése (belső) eljárás

--- GameOfLife/GameOfLife/Noveny/Fu.cs
+++ GameOfLife/GameOfLife/Noveny/Fu.cs
@@ -16,5 +16,13 @@ namespace GameOfLife
                 return tapertek;
             }
         }
+
+        void INoveny.TapertekNoveles()
+        {
+            if (Tapertek < 2)
+            {
+                tapertek++;
+            }
+        }
     }
 }
