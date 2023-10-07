b3ce20d4cb7abc5513eba044e28e12dec6239db2
Csapii <mail.szasz.csaba@gmail.com>
2023-10-07 18:39:04 +0200

    Allat interface meghívása a Nyul osztályba

--- GameOfLife/GameOfLife/Nyul.cs
+++ GameOfLife/GameOfLife/Nyul.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameOfLife.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace GameOfLife
 {
-    internal class Nyul
+    internal class Nyul : IAllat
     {
+        public int JollakottsagiSzint { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+
+        public void Elpusztulas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JollakottsagiSzintBeallitas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool JollakottsagiSzintCsokkentese(int egyseg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool JollakottsagiSzintNovelese(int egyseg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Mozgas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Szaporodas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Taplalkozas()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
