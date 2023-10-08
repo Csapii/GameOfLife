d2cbea6e9c83dae7be1e57b8aac1e247fb1f9bca
HaythFerenc <haythferenc37@gmail.com>
2023-10-08 16:48:55 +0200

    roka jollakottsag

--- GameOfLife/GameOfLife/Roka.cs
+++ GameOfLife/GameOfLife/Roka.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameOfLife.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,66 @@ using System.Threading.Tasks;
 
 namespace GameOfLife
 {
-    internal class Roka
+    internal class Roka : IAllat
     {
+
+        private static int azonositohozSzamlalo = 1;
+        public int Azonosito { get; } = azonositohozSzamlalo++;
+
+        public Roka(int jollakottsagiSzint)
+        {
+            JollakottsagiSzint = jollakottsagiSzint;
+        }
+        public Roka()
+        {
+            ((IAllat)this).JollakottsagiSzintBeallitas();
+        }
+
+        private int jollakottsagiSzint = 10;
+        public int JollakottsagiSzint
+        {
+            get
+            {
+                return jollakottsagiSzint;
+            }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    jollakottsagiSzint = value;
+                }
+                else
+                {
+                    jollakottsagiSzint = 10;
+                }
+            }
+        }
+
+        void IAllat.JollakottsagiSzintBeallitas()
+        {
+            Random rnd = new();
+            int veletlenszam = rnd.Next(0, 11);
+
+            JollakottsagiSzint = veletlenszam;
+        }
+        public bool JollakottsagiSzintNovelese(int egyseg)
+        {
+            if (JollakottsagiSzint + egyseg <= 10 && egyseg > 0)
+            {
+                jollakottsagiSzint += egyseg;
+                return true;
+            }
+            return false;
+        }
+        public bool JollakottsagiSzintCsokkentese()
+        {
+            if (JollakottsagiSzint > 0)
+            {
+                jollakottsagiSzint--;
+                return true;
+            }
+            return false;
+        }
+
     }
 }
