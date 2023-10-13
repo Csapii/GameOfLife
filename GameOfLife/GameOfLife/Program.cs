67d5ed1973ffa2aa5af154ba16c29873d0f2606a
Csorbert <csorbert404@gmail.com>
2023-10-13 18:49:49 +0200

    Bug Fix + Warning Fix

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,1 +1,17 @@
+<<<<<<< Updated upstream
 ﻿Console.WriteLine("Hello, World!");
+=======
+using GameOfLife;
+
+Console.Write("Adja meg a pákya vízszintes méretét (cellákban): ");
+int palyaMeretX = Convert.ToInt32(Console.ReadLine());
+Console.Write("\nAdja meg a pákya vízszintes méretét (cellákban): ");
+int palyaMeretY = Convert.ToInt32(Console.ReadLine());
+Console.Write("\nAdja meg a körök számát: ");
+int korokSzama = Convert.ToInt32(Console.ReadLine());
+
+Palya palya = new (palyaMeretX, palyaMeretY);
+Szimulacio szimulacio = new (palya, korokSzama);
+
+szimulacio.SzimulacioInditas();
+>>>>>>> Stashed changes
