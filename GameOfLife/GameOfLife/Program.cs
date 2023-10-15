63f1ae5f106046335a58f7bc0c933c13fa194a45
Csorbert <csorbert404@gmail.com>
2023-10-15 15:41:15 +0200

    Update Program.cs

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,13 +1,13 @@
 using GameOfLife;
 
-Console.Write("Adja meg a pákya vízszintes méretét (cellákban): ");
+Console.Write("Adja meg a pálya vízszintes méretét (cellákban): ");
 int palyaMeretX = Convert.ToInt32(Console.ReadLine());
-Console.Write("\nAdja meg a pákya vízszintes méretét (cellákban): ");
+Console.Write("\nAdja meg a pálya vízszintes méretét (cellákban): ");
 int palyaMeretY = Convert.ToInt32(Console.ReadLine());
 Console.Write("\nAdja meg a körök számát: ");
 int korokSzama = Convert.ToInt32(Console.ReadLine());
 
-Palya palya = new Palya(palyaMeretX, palyaMeretY);
-Szimulacio szimulacio = new Szimulacio(palya, korokSzama);
+Palya palya = new (palyaMeretX, palyaMeretY);
+Szimulacio szimulacio = new (palya, korokSzama);
 
 szimulacio.SzimulacioInditas();
